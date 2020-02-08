604be45e2ed68b0bfdc3de7ecee36131580a8645
Denver <belleysimon44@gmail.com>
2020-02-07 21:26:06 -0500

    ControllerMenu

--- src/sample/ControllerMenu.java
+++ src/sample/ControllerMenu.java
@@ -6,10 +6,4 @@ import javafx.scene.control.Button;
 public class ControllerMenu {
     @FXML
     Button StartButton, OptionButton, QuitterButton;
-
-    public void setStartButton(Button startButton) {
-        StartButton = startButton;
-    }
-
-    
 }
