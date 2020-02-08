3399fae33ee8ab6d33742e285875483d813333d1
David <david.ibarzabal.1@ulaval.ca>
2020-02-08 09:27:00 -0500

    load button, on mouse click on node

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,28 +2,30 @@ package sample;
 
 
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
-import java.awt.event.MouseEvent;
 import java.io.File;
 
 import javafx.event.Event;
 import javafx.event.EventTarget;
 import javafx.fxml.FXML;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.StackPane;
 
 public class Controller {
     @FXML
     public ImageView iv;
+    @FXML
+    public Circle node1;
+    @FXML
+    public Circle node2;
 
     public void importMap(){
         FileChooser fc = new FileChooser();
@@ -62,5 +64,13 @@ public class Controller {
     public void dragExitMap(){
 
 
+    }
+    public void inputStreet(){
+
+
+    }
+    public void clicNode(MouseEvent event){
+        Circle circle = (Circle)event.getSource();
+        circle.setFill(Color.RED);
     }
 }
