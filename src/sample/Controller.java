6852b75193b336a9eb3ac0054331c13540067525
David <david.ibarzabal.1@ulaval.ca>
2020-02-07 21:16:28 -0500

    ibarbug

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+
+    public Button but;
+
+
+    public void importMap(){
+
+        System.out.println("allo");
+
+        //FileChooser fc = new FileChooser();
+        //fc.setTitle("Veuillez sélectionner un fichier");
+
+        //File fichier = fc.showOpenDialog(null);
+        //Image map1;
+
+        //ImageView imageView = new ImageView(map1);
+
+
+    }
 }
