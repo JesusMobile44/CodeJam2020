bc179be320c6c158a20a64c90a3d53a79afa1982
Denver <belleysimon44@gmail.com>
2020-02-08 16:51:10 -0500

    take this ibar

--- src/sample/Noeud.java
+++ src/sample/Noeud.java
@@ -1,9 +1,12 @@
 package sample;
 
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 import java.util.HashMap;
@@ -13,6 +16,7 @@ public class Noeud extends ImageView{
 
     Tooltip tooltip;
     Boolean selected = false;
+
 
     public HashMap<String, Rue> getRues() {return rues;}
 
@@ -27,20 +31,43 @@ public class Noeud extends ImageView{
     public void setTooltip(Tooltip tooltip) {
         this.tooltip = tooltip;
     }
+
+    public void delete(Pane pane){
+        pane.getChildren().remove(this);
+    }
+
+    public void select(){
+        if (!this.selected){
+            this.selected = true;
+            this.setImage(Main.imagesContainer.get(2));
+        }
+        else{
+            this.selected = false;
+            this.setImage(Main.imagesContainer.get(1));
+        }
+    }
+
     Noeud(ToggleButton buttonToggleNode,ToggleButton buttonToggleRoad, Pane pane){
+
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem delete = new MenuItem();
+        MenuItem select = new MenuItem();
+        delete.setOnAction(event -> delete(pane));
+        delete.setText("Delete");
+        select.setOnAction(event -> select());
+        select.setText("Select");
+        contextMenu.getItems().addAll(select,delete);
+
+        this.setOnContextMenuRequested(event -> {
+            contextMenu.show(this, event.getX(),event.getY());
+        });
+
         this.setOnMouseClicked(event -> {
             if (buttonToggleNode.isSelected() && event.getButton().equals(MouseButton.SECONDARY)){
-                pane.getChildren().remove(this);
+                contextMenu.show(this,this.getX(),this.getY());
             }
             else if (buttonToggleRoad.isSelected()){
-                if (!this.selected){
-                    this.selected = true;
-                    this.setImage(Main.imagesContainer.get(2));
-                }
-                else{
-                    this.selected = false;
-                    this.setImage(Main.imagesContainer.get(1));
-                }
+                select();
             }
         });
     }
