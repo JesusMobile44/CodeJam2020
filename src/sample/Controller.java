f26df8c8a4a8f93685166f003f170d34ae2623a3
David <david.ibarzabal.1@ulaval.ca>
2020-02-08 08:31:52 -0500

    try pull

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,9 +5,11 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
+import java.awt.event.MouseEvent;
 import java.io.File;
 
 public class Controller {
@@ -21,4 +23,5 @@ public class Controller {
         Image map = new Image(fichier.toURI().toString());
         iv.setImage(map);
     }
+   
 }
